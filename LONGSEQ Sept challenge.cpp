687dc8140f15e67deda34cc3c40b3eb07a9c9804
Deepak Garg <deepakgarg0802@gmail.com>
2016-09-02 22:43:51 +0530

    codechef
    Chef has a number D containing only digits 0's and 1's. He wants to make the number to have all the digits same. For that, he will change exactly one digit, i.e. from 0 to 1 or from 1 to 0. If it is possible to make all digits equal (either all 0's or all 1's) by flipping exactly 1 digit then output "Yes", else print "No" (quotes for clarity)

---
+++ LONGSEQ Sept challenge.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes her
+	
+	int t,num1,num0,n;
+	char str[100000];
+	cin>>t;
+	
+	while(t--)
+	{
+		num1=num0=0;
+		n=0;
+		int i=0;
+		cin>>str;
+		while(str[i] != '\0')
+		{
+			//cout<<str[i];
+			if(str[i]=='1')
+				num1++;
+			else if(str[i]=='0')
+				num0++;
+				
+			n++;
+			i++;
+		}
+		 if(num1==1 || num0==1)
+		{
+			cout<<"Yes\n";
+		}
+		else
+		{
+			cout<<"No\n";
+		}
+		
+	}
+	
+	return 0;
+}
