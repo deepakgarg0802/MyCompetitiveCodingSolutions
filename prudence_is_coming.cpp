fd23d7f7110bb2f53d57f701d98a17572cfe48a3
Deepak Garg <deepakgarg0802@gmail.com>
2017-01-15 20:08:38 +0530

    Add : Hackerrank (sintaxis-experto)
    Well to get you started on Leaderboard and make you comfortable with the environment, here is a very simple problem.
    
    Scan name of your friend and tell him "Hey (Name of Friend), Prudence 2017 is coming.".

---
+++ prudence_is_coming.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    char ch[200];
+    gets(ch);
+    cout<<"Hey "<<ch<<", Prudence 2017 is coming.";
+    return 0;
+}
